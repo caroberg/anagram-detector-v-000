0593b26b75be4068c51776ef95f90a7fd5fb3c8a
Caroline Berg <carolineeberg@gmail.com>
2018-08-29 14:21:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(find_anagram)
+    find_anagram.find_all do |words|
+      if words.split("").sort == self.word.split
+        ("").sort
+          word
+        end
+      end
+  end
+end
